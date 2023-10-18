429c0d350343bdb4055e83eb540620bf1c182306
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-17 22:54:28 -0300

    feat: parsing URL search params

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -3,13 +3,16 @@ import type {
   LoaderFunction,
   MetaFunction,
 } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { Film } from "../api/films";
 import { getFilms } from "../api/films";
 
 // SERVER SIDE
-export const loader: LoaderFunction = async () => {
-  return getFilms();
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title");
+
+  return getFilms(title);
 };
 
 // CLIENT SIDE
@@ -19,7 +22,7 @@ export default function FilmsIndex() {
   return (
     <div className="p-16 font-sans">
       <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
-      <form className="py-5">
+      <Form reloadDocument method="get" className="py-5">
         <label className="font-bold">
           Search{" "}
           <input
@@ -35,7 +38,7 @@ export default function FilmsIndex() {
         >
           Search
         </button>
-      </form>
+      </Form>
       <div className="grid grid-cols-4 gap-4">
         {films.map((film) => (
           <div
